a3cb6fb341c785bc67a559eec2cb5328b896e4c9
Пользователь <arsnurperi@gmail.com>
2021-07-07 18:07:55 +0600

    added table and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
-import React from "react";
-import "./App.less";
-const App = () => <div className="App">IMT Calculator</div>;
-export default App;
+import React from 'react'
+import HomePage from './components/HomePage/index.jsx'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import './App.less'
+import Calculator from './components/Calculator/index.jsx'
+
+const App = () => (
+	<div className='App'>
+		<h1>IMT Calculator</h1>
+		<Router>
+			<Switch>
+				<Route path='/calculator'>
+					<Calculator />
+				</Route>
+				<Route path='/'>
+					<HomePage />
+				</Route>
+			</Switch>
+		</Router>
+	</div>
+)
+export default App
